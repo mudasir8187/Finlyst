44ef119ac300d186cc8cfd7f6a251d6c6e6760de
Mudasir Ali <109215785+mudasir8187@users.noreply.github.com>
2025-08-20 14:21:51 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -2,8 +2,8 @@ import os
 import re
 import streamlit as st
 # ---------- Configuration (use environment variables) ----------
-DB_HOST = "123.45.67.89"  #st.secrets["db_host"] #os.getenv("DB_HOST", "localhost")
-DB_PORT = 5432 #st.secrets["db_port"]  #os.getenv("DB_PORT", "5432")
+DB_HOST = st.secrets["db_host"] #os.getenv("DB_HOST", "localhost")
+DB_PORT = st.secrets["db_port"]  #os.getenv("DB_PORT", "5432")
 DB_ADMIN_USER = st.secrets["db_user"]  #os.getenv("DB_ADMIN_USER", "postgres")
 DB_ADMIN_PWD = st.secrets["db_password"]  # os.getenv("DB_ADMIN_PWD", "moni123")
 
@@ -14,4 +14,5 @@ DB_APP_PWD = st.secrets["db_password"] #os.getenv("DB_APP_PWD", DB_ADMIN_PWD)
 # Limits
 MAX_UPLOAD_BYTES = 200 * 1024 * 1024 #int(os.getenv("MAX_UPLOAD_BYTES", 200 * 1024 * 1024))
 ALLOWED_NAME_RE = re.compile(r"^[a-z0-9_]+$")
+
 
